b8423e1b2f4285c1085fc901bd9fa1a2685d2ce5
Darius Runge <71567459+drn1998@users.noreply.github.com>
2020-09-21 18:46:49 +0200

    Deserialize, calculate hash, ...
    Added a lot of fundamental functions required for the final function library to work properly.

--- src/hcommit.c
+++ src/hcommit.c
@@ -1,3 +1,9 @@
+/* General Todo
+ * Is there a glib-internal function to test if an object (especially strings) is set or not?
+ * Rename SDTP_commitment_write to **_serialize?
+ * Use glibcryptos implementation for SHA-256 and random entropy
+ */
+
 #include <glib-2.0/glib.h>
 #include <stdio.h>
 
@@ -62,10 +68,21 @@ int SDTP_commitment_content_unset(commitment_s * obj) {
 }
 
 int SDTP_commitment_calculate(commitment_s * obj) {
-    puts("Warning: Hash generation unimplemented. Only assigning random entropy!");
+    // TODO: Check if time, msg and subject is set already
+
+    GByteArray * data_to_hash;
+
+    data_to_hash = g_byte_array_new();
+
+    guint8 hash[32];
 
     guint8 entropy[DEF_ENTROPY_LENGTH];
     FILE * entropy_f;
+    #ifdef DEBUG
+    FILE * hashout_f;
+
+    hashout_f = fopen("hash", "w");
+    #endif
 
     entropy_f = fopen("/dev/urandom", "r");
 
@@ -78,21 +95,73 @@ int SDTP_commitment_calculate(commitment_s * obj) {
     fclose(entropy_f);
 
     g_byte_array_append(obj->commitment_entropy, entropy, sizeof(entropy));
+
+    SDTP_commitment_serialize(obj, data_to_hash);
+
+    calc_sha_256(hash, data_to_hash->data, data_to_hash->len);
+
+    g_byte_array_append(obj->commitment_hashval, hash, sizeof(hash));
+
+    #ifdef DEBUG
+    fwrite(obj->commitment_hashval->data, obj->commitment_hashval->len, 1, hashout_f);
+
+    fclose(hashout_f);
+    #endif
+
+    g_byte_array_free(data_to_hash, TRUE);
+    return 0;
 }
 
 /* Distinguish between commit and reveal out = reveal... OR let this be a mere
 generator of the block to be hashed so it can later be performed with the reveal file
 (and the commit file in the beginning) for comparison. */
 
-int SDTP_commitment_write(commitment_s * obj, GByteArray * out) {
-    gint64 rdate;
+int SDTP_commitment_serialize(commitment_s * obj, GByteArray * out) {
+    gint64 rdate;   // CHECK Unsigned?
 
     rdate = GINT64_TO_BE(g_date_time_to_unix(obj->commitment_revelation));
 
     g_byte_array_append(out, obj->commitment_entropy->data, obj->commitment_entropy->len);
+    g_byte_array_append(out, &rdate, sizeof(rdate));
     g_byte_array_append(out, obj->commitment_subject->str, strlen(obj->commitment_subject->str) + 1);
     g_byte_array_append(out, obj->commitment_message->str, strlen(obj->commitment_message->str) + 1);
-    g_byte_array_append(out, &rdate, sizeof(rdate));
+
+    return 0;
+}
+
+int SDTP_commitment_deserialize(commitment_s * obj, GByteArray * in) {
+    gint64 rdate = 0;
+    guint32 entropy_o = 0;
+    guint32 revelation_o = entropy_o + DEF_ENTROPY_LENGTH;
+    guint32 subject_o = revelation_o + sizeof(rdate);
+    guint32 message_o; // Undefined
+
+    // assert that in-> len has minimum length
+
+    g_byte_array_remove_range(obj->commitment_entropy, 0, obj->commitment_entropy->len);
+    g_byte_array_append(obj->commitment_entropy, in, DEF_ENTROPY_LENGTH);
+
+    memcpy(&rdate, in->data + revelation_o, 8);
+    rdate = GINT64_FROM_BE(rdate);
+
+    if (obj->commitment_schedule_b)
+        g_date_time_unref(obj->commitment_revelation);
+
+    obj->commitment_revelation = g_date_time_new_from_unix_utc(rdate);
+
+    #ifdef DEBUG
+    //printf("%lx", rdate);
+    #endif
+
+    if (obj->commitment_subject_b) {
+        g_string_assign(obj->commitment_subject, in->data + subject_o);
+    }
+
+    message_o = subject_o + strlen(obj->commitment_subject->str) + 1;
+
+    if (obj->commitment_message_b) {
+        g_string_assign(obj->commitment_message, in->data + message_o);
+    }
 
     return 0;
 }
@@ -128,6 +197,7 @@ int SDTP_commitment_prepare(commitment_s * obj) {
     // TODO Security measure so it's called once?
 
     obj->commitment_entropy = g_byte_array_new();
+    obj->commitment_hashval = g_byte_array_new();
 
     obj->commitment_message_b = FALSE;
     obj->commitment_payload_b = FALSE;
@@ -146,6 +216,7 @@ int SDTP_commitment_clear(commitment_s * obj) {
         g_string_free(obj->commitment_message, TRUE);
 
     g_byte_array_free(obj->commitment_entropy, TRUE);
+    g_byte_array_free(obj->commitment_hashval, TRUE);
 
     return 0;
 }
